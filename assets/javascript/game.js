2edb9f82bf47009e58178a543b7d6689d2b508b4
Balerina <katarzynab9236@gmail.com>
2018-09-27 01:31:57 -0500

    second hw update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,40 @@
-//Define Global Variables
+//Define Global Variables 
 
-//Create a game start event with 
+
+
+//Create a function that assigns numbers to the buttons
+function gameReset () {
+    wins = 0;
+    losses = 0;
+    randomNumber();
+    randomButtons();
+    $("#wins, #losses, #user-number").empty();
+}
+
+gameReset()
+
 
 //Create a computer generated random number function
+function randomNumber() {
+    computerRandom = Math.floor(Math.random( ) * 65 + 35);
+    $("#computer-random").text(computerRandom);
+};
 
-//
+//Create a function that assigns random numbers to all buttons 
+function randomButtons() {
+    randomBtn1 = Math.floor(Math.random() * 19 + 1);
+    randomBtn2 = Math.floor(Math.random() * 19 + 1);
+    randomBtn3 = Math.floor(Math.random() * 19 + 1);
+    randomBtn4 = Math.floor(Math.random() * 19 + 1);
+}
 
+randomButtons();
+
+
+//Create an "ON CLICK" event that gives and displays the buttons random values
+$("#btn1").on("click", function() {
+    result+= parseInt(randomBtn1);
+    $("#user-number").text(result);
+    console.log("btn1: ", randomBtn1, "user-number:", result);
+
+});
