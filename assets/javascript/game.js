0bdb471dc1088be9e8e4f50d2bd527a23d7e96bb
Balerina <katarzynab9236@gmail.com>
2018-09-27 18:43:48 -0500

    update javascript bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,27 @@
 //Define Global Variables 
-
-
+var computerRandom;
+var randomBtn1;
+var randomBtn2;
+var randomBtn3;
+var randomBtn4;
+var result = 0;
+var wins = 0;
+var losses = 0;
 
 //Create a function that assigns numbers to the buttons
 function gameReset () {
-    wins = 0;
-    losses = 0;
     randomNumber();
     randomButtons();
-    $("#wins, #losses, #user-number").empty();
+    result = 0;
 }
 
-gameReset()
+gameReset();
 
 
 //Create a computer generated random number function
 function randomNumber() {
     computerRandom = Math.floor(Math.random( ) * 65 + 35);
-    $("#computer-random").text(computerRandom);
+    $("#match-number").text(" " + computerRandom);
 };
 
 //Create a function that assigns random numbers to all buttons 
@@ -32,9 +36,49 @@ randomButtons();
 
 
 //Create an "ON CLICK" event that gives and displays the buttons random values
-$("#btn1").on("click", function() {
-    result+= parseInt(randomBtn1);
-    $("#user-number").text(result);
-    console.log("btn1: ", randomBtn1, "user-number:", result);
+function run() {
 
+    
+$("#btn1").on("click", function() {
+    result+= (randomBtn1);
+    $("#player-number").text(" " + result);
+    if (result > computerRandom) {
+        checkWin(); }
+
+    console.log("btn1: ", randomBtn1, "user-number:", result);
+}); 
+
+$("#btn2").on("click", function() {
+    result+= (randomBtn2);
+    $("#player-number").text(" " + result);
+    if (result > computerRandom) {
+        checkWin(); }
 });
+
+$("#btn3").on("click", function() {
+    result+= (randomBtn3);
+    $("#player-number").text(" " + result);
+    if (result > computerRandom) {
+        checkWin(); }
+});
+
+$("#btn4").on("click", function() {
+    result+= (randomBtn4);
+    $("#player-number").text(" " + result);
+    if (result > computerRandom) {
+        checkWin(); }
+});
+};
+
+function checkWin() {
+if (result === computerRandom) {
+    wins++;
+$("#player-wins").text(wins);
+} else if (result > computerRandom) {
+    losses++;
+$("#player-losses").text(losses);
+    gameReset();
+}
+}
+
+run();
